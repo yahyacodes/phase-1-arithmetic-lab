ed473537d430ff5e6e9463fe73f61105548e71a0
yahyacodes <yahya.codin@gmail.com>
2023-09-18 15:21:32 +0300

    m

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 18, 20);
